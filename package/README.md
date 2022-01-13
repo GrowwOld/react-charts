04e7e8ecb03cfce8c494a3d2ec5368c30a4a9817
samyak047 <samyakjainmnv@gmail.com>
2022-01-13 10:52:47 +0530

    init: build generated

---
+++ package/README.md
@@ -0,0 +1,2 @@
+# react-charts
+We have created this small library specifically for charting needs of Groww.
---
+++ package/index.js
@@ -0,0 +1,5 @@
+// export {default as BarGraph} from './src/BarGraph'
+export {default as LineGraph} from './src/LineGraph';
+export {default as CandleChart} from './src/CandleChart';
+export {default as PieChart} from './src/PieChart';
+
